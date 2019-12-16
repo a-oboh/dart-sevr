fd6da95bb3ded493838864087f57b463e6c3d6b1
WinningGreat <winninggreat@gmail.com>
2019-12-16 07:48:39 +0100

    add file upload example

---
+++ example/example_file_upload.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:sevr/sevr.dart';
+
+// final file2 = File(req.files['lolag3']['filename']);
+// req.files['lolag']['buffer'].stream.listen( (data){
+// print(data);
+// });
+main() {
+  var serv = Sevr();
+  serv.get('/test', [
+    (req, res) async {
+      for (int i = 0; i < req.files.keys.length; i++) {
+        //Handle your file stream as you ssee fit, write to file, pipe to a cdn etc --->
+        File file = File(req.files[req.files.keys.toList()[i]].filename);
+        await for (var data
+            in req.files[req.files.keys.toList()[i]].streamController.stream) {
+              if(data is String){
+                await file.writeAsString(data, mode: FileMode.append);
+              } else{
+                 await file.writeAsBytes(data, mode: FileMode.append);
+              }
+         
+        }
+      }
+
+      return res.status(200).json(req.body);
+    }
+  ]);
+
+  serv.listen(3000, callback: () {
+    print('Listening on ${3000}');
+  });
+}
